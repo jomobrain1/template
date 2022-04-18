64acda1db5ed40e9d89cb774fe06d001102ed5b1
unknown <jomobrain1@gmail.com>
2022-04-18 05:48:25 -0700

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// let cat=document.getElementById("cat")
+// let output=document.getElementById("output")
+// console.log(cat,"Categ",output)
+
+// fetch('https://comtra.co.ke/api/categories')
+// .then(res=>res.json())
+// .then(data=>{
+//     console.log(data)
+//     let out=""
+//     data.forEach(item=> {
+//         console.log(item.name)
+
+      
+//         console.log(item.name)
+//         out+=`
+//         <li>${item.name}</li>
+//         `
+//         // out+=`
+//         // <div class="icon">
+//         // <a href="http://instagram.com">
+//         // <h6 id="cat">${item.name}</h6>
+//         // <i class="fa fa-instagram"></i>
+//         // </a>
+//         // </div>
+            
+        
+//         // `
+        
+       
+//       document.getElementById('dt').innerHTML=out
+        
+//     });
+// })
